2f283f2aa95b3a61114c134630b09228768e4fe6
Kaylen Dart <actuallyori@gmail.com>
2023-04-25 17:06:18 +0100

    feat: create basic prisma schema

--- crates/prisma/src/lib.rs
+++ crates/prisma/src/lib.rs
@@ -1,14 +1,7 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+//! # howlapp-prisma
+//!
+//! Exports the generated Prisma client for Howl services.
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+mod generated;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub use generated::*;
