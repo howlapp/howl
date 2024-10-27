b70369f30a2677f785d5731d1d53d7a46c59dfa9
kaylendog <actuallyori@gmail.com>
2024-10-27 11:48:59 +0000

    chore: add ignores

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,9 @@ import infernal from "eslint-config-infernal";
 export default [
 	...infernal,
 	{
+		ignores: ["packages/eslint-config-infernal/*", "packages/prettier-config-infernal/*"],
+	},
+	{
 		languageOptions: {
 			parserOptions: {
 				projectService: true,
