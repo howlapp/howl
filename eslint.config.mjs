fe9c075f29c385affc48a546bcab0ca8d115c354
kaylendog <actuallyori@gmail.com>
2024-10-27 11:43:58 +0000

    chore: update packages; eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,13 @@
 import infernal from "eslint-config-infernal";
 
-export default [infernal];
+export default [
+	...infernal,
+	{
+		languageOptions: {
+			parserOptions: {
+				projectService: true,
+				tsconfigRootDir: import.meta.dirname,
+			},
+		},
+	},
+];
